9baa483bb141b61dbdb204233b6166594d48a384
theddy9098 <teddyfso15@gmail.com>
2025-09-01 22:01:47 -0500

    Operacion de suma de  la matriz

--- matrizes.py
+++ matrizes.py
@@ -1,8 +1,11 @@
 Matriz = [[1,2,3],[4,5,6],[7,8,9]]
 
-for j in range(len(Matriz[0])):
-    for i in range(len(Matriz)):
-        print(Matriz[i][j], end=" ")
-    print()
+suma = 0
+
+for Elemento in Matriz:
+    for val   in Elemento:
+        suma += val
+
+print("La suma de los elementos es:",suma)
 
 
