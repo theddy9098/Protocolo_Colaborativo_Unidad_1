19da0e823c528035743301f7744c300c8c703346
theddy9098 <teddyfso15@gmail.com>
2025-09-02 22:12:11 -0500

    Busqueda Lineal para encontrar la pocosion

--- TypeScript/ejercicios.ts
+++ TypeScript/ejercicios.ts
@@ -1,27 +1,50 @@
-const Numeros: number[]=[2,3,6,7,10,12,4,9,5,3];
+const Numeros: number[] = [2, 3, 6, 7, 10, 12, 4, 9, 5, 3];
 
 console.log("Los numeros son: " + Numeros);
 
 console.log("\n ")
 
-for (let i=0; i < Numeros.length; i++){
-    if (i % 2 != 0){
-        console.log(Numeros [i] = 0 );
+for (let i = 0; i < Numeros.length; i++) {
+    if (i % 2 != 0) {
+        Numeros[i] = 0;
     }
 }
-console.log("\n")
+console.log("\n");
 
-for (let i of Numeros){
+for (let i of Numeros) {
     console.log("Los numeros son: " + i );
     
 }    
 console.log("\n")
 
-for(let i=0; i < Numeros.length; i++){
+for (let i = 0; i < Numeros.length; i++) {
     Numeros[i] = Numeros[i] * i;    
 }
 
-console.log("Los numeros multipliacso son: \n ");
+console.log("Los numeros multiplicados son: \n ");
 for (let i of Numeros){
     console.log(i);
 }
+
+
+function busquedaLineal(inx: number[], valor: number): number {
+    for (let i = 0; i < inx.length; i++) {
+        if (inx[i] === valor) {
+            return i;
+        }
+    }
+    return -1; 
+}
+
+
+const valorBuscado = 24; 
+const resultado = busquedaLineal(Numeros, valorBuscado);
+
+if (resultado !== -1) {
+    console.log("El valor "+ valorBuscado +" se encuentra en el índice " + resultado);
+} else {
+    console.log("El valor" + valorBuscado + "no se encontro ");
+}
+
+
+
