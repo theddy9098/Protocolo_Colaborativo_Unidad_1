139e7d37a1b6bb1245811e132eb829ad87f8681e
theddy9098 <teddyfso15@gmail.com>
2025-09-02 19:36:45 -0500

    Recorrido con el bucle for-each

--- TypeScript/ejercicios.ts
+++ TypeScript/ejercicios.ts
@@ -1,7 +1,7 @@
-const Nummeros: number[]=[2,4,6,8,10,12,4,9,5,3];
+const Numeros: number[]=[2,4,6,8,10,12,4,9,5,3];
 
 //Recorrido con el for clasico
 
-for (let i=0; i<Nummeros.length; i++){
-    console.log(Nummeros[i]);
+for (let i of Numeros){
+    console.log(i);
 }
